0f0441c113dbfa5fe156ec193deac261c7ffcfe9
Joni0086 <jonasnil86@gmail.com>
2019-12-01 04:59:54 +0100

    BT31

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -30,6 +30,7 @@ namespace DatingApp.API
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddCors(); //< Adds cores middleware, used for permission like features for the front/backend.
             services.AddControllers();
+            services.AddScoped<IAuthRepository, AuthRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
