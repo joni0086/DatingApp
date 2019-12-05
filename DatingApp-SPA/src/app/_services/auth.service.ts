eb54e653f1ba4df5c7d2474deca521b010cd395f
Joni0086 <jonasnil86@gmail.com>
2019-12-05 10:40:58 +0100

    End of section 6

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {map} from 'rxjs/operators';
+import {JwtHelperService} from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   baseUrl = 'http://localhost:5000/api/auth/';
-
+  jwtHelper = new JwtHelperService();
+  decodedToken: any;
+  
   constructor(private http: HttpClient) { }
 
   login(model: any) {
@@ -16,6 +19,8 @@ export class AuthService {
         const user = response;
         if (user) {
           localStorage.setItem('token', user.token);
+          this.decodedToken = this.jwtHelper.decodeToken(user.token);
+          console.log(this.decodedToken);
         }
       })
     );
@@ -24,4 +29,12 @@ export class AuthService {
   register(model: any) {
     return this.http.post(this.baseUrl + 'register', model);
   }
+
+  // Method that checks if a token has expired, as well as if the user is logged in.
+  // Returns true or false.
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token);
+  }
+
 }
